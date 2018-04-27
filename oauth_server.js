08302535c4f1ec66cae425bb5654adb85ee76ae2
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-04-26 22:53:45 -0500

    sync

--- oauth_server.js
+++ oauth_server.js
@@ -476,3 +476,31 @@ OAuth.openSecrets = function (serviceData, userId) {
   });
   return result;
 };
+
+
+
+// connect middleware
+OAuth._requestHandlers['2'] = function (service, query, res) {
+  // check if user authorized access
+  if (!query.error) {
+    // Prepare the login results before returning.
+
+    // Run service-specific handler.
+    var oauthResult = service.handleOauthRequest(query);
+    var credentialSecret = Random.secret();
+
+    var credentialToken = OAuth._credentialTokenFromQuery(query);
+
+    // Store the login result so it can be retrieved in another
+    // browser tab by the result handler
+    OAuth._storePendingCredential(credentialToken, {
+      serviceName: service.serviceName,
+      serviceData: oauthResult.serviceData,
+      options: oauthResult.options
+    }, credentialSecret);
+  }
+
+  // Either close the window, redirect, or render nothing
+  // if all else fails
+  OAuth._renderOauthResults(res, query, credentialSecret);
+};
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'clinical:oauth',
   summary: "Common code for OAuth-based services in the clinical setting.",
-  version: "1.2.5",
+  version: "1.2.6",
   git: 'https://github.com/clinical-meteor/oauth'
 });
 
