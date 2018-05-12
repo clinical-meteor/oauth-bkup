03758202b9ce57ef3112bf14c710ccb390e78bd1
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-05-11 20:33:24 -0500

    OAuth2.serviceName

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'clinical:oauth',
   summary: "Common code for OAuth-based services in the clinical setting.",
-  version: "1.2.18",
+  version: "1.2.19",
   git: 'https://github.com/clinical-meteor/oauth'
 });
 
